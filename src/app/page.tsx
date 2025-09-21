9e079c088230b2e3ccf65b051c8a894a149e66e5
axyklee <58136818+aaronleetw@users.noreply.github.com>
2025-09-21 21:19:28 +0800

    feat: if logged in, redirect to admin page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import { HydrateClient } from "~/trpc/server";
 import Header from "./_components/header";
+import { auth } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await auth();
+
+  if (session) {
+    redirect("/admin");
+  }
+
   return (
     <HydrateClient>
       <Header panel={null}>
