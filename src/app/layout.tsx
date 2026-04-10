c0f77446454fc52a966c3da55e31916921bb9e66
axyklee <longyklee@gmail.com>
2026-04-10 20:52:34 +0800

    feat: add favicon and web app icons with manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
+      <head>
+        {/* icons */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="bg-neutral-50">
         <TRPCReactProvider>{children}</TRPCReactProvider>
         <script defer src="https://cloud.umami.is/script.js" data-website-id="de77df28-58bf-43e8-aada-e56af5ff7e44"></script>
